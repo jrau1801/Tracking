d17dae43975eb8c87c8532290a80757332d14c6b
jrau1801 <janrau1802@gmail.com>
2023-12-05 04:44:30 +0100

    added iou and section output

--- src/cam_sections.cpp
+++ src/cam_sections.cpp
@@ -12,8 +12,8 @@ int main() {
     }
 
     // Check the camera properties and set the width and height of the capture frame
-    cap.set(CAP_PROP_FRAME_WIDTH, 640);
-    cap.set(CAP_PROP_FRAME_HEIGHT, 480);
+    cap.set(CAP_PROP_FRAME_WIDTH, 1600);
+    cap.set(CAP_PROP_FRAME_HEIGHT, 900);
 
     // Create a window to display the output
     namedWindow("Split Frame", WINDOW_NORMAL);
@@ -27,7 +27,7 @@ int main() {
     int randomX = rand() % (width - (width / 3)); // Random x-value within the frame width
 
     // Fourth bounding box coordinates
-    Rect boundingBox(randomX, height / 4, width / 3, height / 2);
+    Rect boundingBox(800, height /4, width / 3, height / 2);
 
     while (true) {
         Mat frame;
@@ -43,12 +43,40 @@ int main() {
         Rect section2(width / 3, 0, width / 3, height);
         Rect section3((width / 3) * 2, 0, width / 3, height);
 
-        // Draw hollow rectangles with different colors in each section
+        // Calculate intersection areas
+        double intersection1 = (section1 & boundingBox).area();
+        double intersection2 = (section2 & boundingBox).area();
+        double intersection3 = (section3 & boundingBox).area();
+
+        // Calculate union areas
+        double union1 = section1.area() + boundingBox.area() - intersection1;
+        double union2 = section2.area() + boundingBox.area() - intersection2;
+        double union3 = section3.area() + boundingBox.area() - intersection3;
+
+        // Calculate IoU values
+        double iou1 = intersection1 / union1;
+        double iou2 = intersection2 / union2;
+        double iou3 = intersection3 / union3;
+
+        // Determine which section the bounding box belongs to based on the highest IoU
+        int section = 0;
+        double max_iou = std::max({iou1, iou2, iou3});
+        if (max_iou == iou1) {
+            section = 1;
+        } else if (max_iou == iou2) {
+            section = 2;
+        } else if (max_iou == iou3) {
+            section = 3;
+        }
+
+        // Draw the text indicating the section
+        std::string section_text = "Section: " + std::to_string(section);
+        putText(frame, section_text, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1, Scalar(255, 255, 255), 2);
+
+        // Draw the rectangles for sections and bounding box
         rectangle(frame, section1, Scalar(255, 0, 0), 2); // Blue, thickness = 2
         rectangle(frame, section2, Scalar(0, 255, 0), 2); // Green, thickness = 2
         rectangle(frame, section3, Scalar(0, 0, 255), 2); // Red, thickness = 2
-
-        // Draw the fixed fourth bounding box
         rectangle(frame, boundingBox, Scalar(255, 255, 0), 2); // Yellow, thickness = 2
 
         // Display the frame
